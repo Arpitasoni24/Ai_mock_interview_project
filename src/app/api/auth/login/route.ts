0039eb18b3319b0933dc62d29eca3883c5b13a84
Arpita Soni <soniarpita954@gmail.com>
2026-03-27 18:03:48 +0530

    authFixed

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -73,9 +73,9 @@ export async function POST(req: Request) {
     //   httpOnly: true,
     //   secure: process.env.NODE_ENV === "production",
     //   sameSite: "strict",
-    httpOnly: false,
-      secure: false,
-      sameSite: "lax",  
+    httpOnly: true,
+      secure: true,
+      sameSite: "none",  
       path: "/",
       maxAge: 60 * 60 * 24 * 7, // 7 days
     });
