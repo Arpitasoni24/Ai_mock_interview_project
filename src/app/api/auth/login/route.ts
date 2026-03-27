40e4f846a9cf90ff63318055f8b852e17839e794
Arpita Soni <soniarpita954@gmail.com>
2026-03-27 18:43:44 +0530

    fixedLogin

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -75,7 +75,7 @@ export async function POST(req: Request) {
     //   sameSite: "strict",
     httpOnly: true,
       secure: true,
-      sameSite: "none",  
+      sameSite: "lax",  
       path: "/",
       maxAge: 60 * 60 * 24 * 7, // 7 days
     });
